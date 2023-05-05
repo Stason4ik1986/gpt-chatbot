b367f3ef97220eee6dde0e644e9fd39fa4d1cb98
Stanislav Cherednychenko <stanislav.cherednychenko@jelvix.com>
2023-05-05 10:39:12 +0300

    Updated OPEN AI Key

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ dotenv.config({ path: `${APP_ROOT_PATH}.env.${process.env.NODE_ENV}` }); // chan
 
 export const CONFIG = {
   NODE_ENV: process.env.NODE_ENV ?? 'development',
-  OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY ?? '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',
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN ?? '5866922307:AAEVE7r5ORww66eHIL9hSMzIvjSTznBaDTQ',
+  OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
 };
 
